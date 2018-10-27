95ffead2a5a1797cf31dcb6b08ca928d974c47b5
DamperX <gosu_1994@mail.ru>
2018-10-27 12:00:10 +0300

    Исправил ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 "use strict";
 
 var gulp = require("gulp");
+var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var svgstore = require('gulp-svgstore');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -15,8 +19,10 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"));
+  });
 
 gulp.task("server", function () {
   server.init({
@@ -25,6 +31,23 @@ gulp.task("server", function () {
     open: true,
     cors: true,
     ui: false
+  });
+
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png, jpg, svg}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("source/img"));
+  });
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("icon-sprite.svg"))
+    .pipe(gulp.dest("source/img"));
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
